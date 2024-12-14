a2ea6dc65d8e1161f3413f8ec1edde9c3d1dd7ad
Madalena <madalena.mota@tecnico.ulisboa.pt>
2024-12-14 14:22:35 +0000

    um pouco do input

--- Projeto2/Projeto2.cpp
+++ Projeto2/Projeto2.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
 using namespace std;
 
+/*
+visited = []
+queue = []
+
+def bfs(visited, graph, node): #function for BFS
+  visited.append(node)
+  queue.append(node)
+  distance = []
+
+  while queue != empty:          # Creating loop to visit each node
+    m = queue.pop(0) 
+
+    for neighbour in graph[m]:
+      if neighbour not in visited:
+        visited.append(neighbour)
+        queue.append(neighbour)
+        distance[neighbour] = distance[parent]+1
+*/
+
 int main() {
+    // n estacoes, m arcos, l linhas
+    int n, m, l;
+    scanf("%d%d%d", &n, &m, &l);
+
+    
 
     return 0;
 }
