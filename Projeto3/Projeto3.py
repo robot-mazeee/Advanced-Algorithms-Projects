27821006a666dcf9361969477a9ec3ceaeb0658f
Madalena <madalena.mota@tecnico.ulisboa.pt>
2025-01-08 13:37:22 +0000

    projeto feito

--- Projeto3/Projeto3.py
+++ Projeto3/Projeto3.py
@@ -1,5 +1,7 @@
 import pulp as p
 
+# ola stor :)
+
 '''
 1) variaveis do problema
 
@@ -21,10 +23,6 @@ respeitando as restrições do problema
 # problema
 problem = p.LpProblem("P3", p.LpMaximize)
 
-fabrica_to_pais = {}
-fabricas_crianca = {}
-crianca_to_pais = {}
-
 # numero total de fabricas, paises e criancas
 n, m, t = input().split()
 n = int(n)
@@ -33,46 +31,59 @@ t = int(t)
 
 # names
 n_names = [str(i) for i in range(1, n+1)]
-m_names = [str(i) for i in range(1, m+1)]
 t_names = [str(i) for i in range(1, t+1)]
 
-# vars
-n_vars = p.LpVariable.dicts("fabricas", n_names, 0, None, p.LpInteger)
-m_vars = p.LpVariable.dicts("paises", m_names, None, None, p.LpInteger)
-t_vars = p.LpVariable.dicts("criancas", t_names, 0, None, p.LpInteger)
-x = p.LpVariable.dicts('x', (t_names, n_names), 0, 1, p.LpBinary)
-x = {(t, n): x[t][n] for t in t_names for n in n_names}
+# dicionarios
+fabrica_to_pais = {}
+fabrica_max = {}
+pais_min = {}
+pais_max = {}
+
+pais_to_export = {var: 0 for var in range(1, m+1)}
+pais_to_non_export = {var: 0 for var in range(1, m+1)}
+fabrica_to_stock = {var: 0 for var in n_names}
 
 # loop das fabricas
 for _ in range(n):
     # fabrica, pais, stock
     fabrica, pais, stock = input().split()
-    n_vars[fabrica].upBound = int(stock)
+    fabrica_max[fabrica] = int(stock)
     fabrica_to_pais[fabrica] = pais
 
 # loops dos paises
 for _ in range(m):
     pais, pmaxj, pminj = input().split()
-    m_vars[pais].lowBound = int(pminj)
-    m_vars[pais].upBound = int(pmaxj)
+    pais_min[int(pais)] = int(pminj)
+    pais_max[int(pais)] = int(pmaxj)
 
 # loop das criancas
+funcao_objetivo = 0
 for _ in range(t):
     crianca, pais, *fabricas = input().split()
-    fabricas_crianca[crianca] = fabricas
-    crianca_to_pais[crianca] = pais
-    problem += p.lpSum(x[(crianca, fabrica)] for fabrica in fabricas) <= 1
+
+    x = p.LpVariable.dicts('x', [(int(crianca), int(fabrica)) for fabrica in fabricas], 0, 1, p.LpBinary)
+
+    gifts_received = 0
+    for fabrica in fabricas:
+        value = x[(int(crianca), int(fabrica))]
+        gifts_received += value
+        funcao_objetivo += value
+        fabrica_to_stock[fabrica] += value
+        pais_to_non_export[int(pais)] += value
+        if fabrica_to_pais[fabrica] != pais:
+            pais_to_export[int(fabrica_to_pais[fabrica])] += value
+            
+    problem += gifts_received <= 1
+
+for pais in range(1, m+1):
+    problem += pais_to_non_export[pais] >= pais_min[pais]
+    problem += pais_to_export[pais] <= pais_max[pais]
 
 for fabrica in n_names:
-    problem += p.lpSum(x[(crianca, fabrica)] for crianca in t_names if fabrica in fabricas_crianca[crianca]) <= n_vars[fabrica].upBound
-
-for pais in m_names:    
-    problem += p.lpSum(x[(crianca, fabrica)] for crianca in t_names for fabrica in fabricas_crianca[crianca] if crianca_to_pais[crianca] == pais) >= m_vars[pais].lowBound 
-
-    problem += p.lpSum(x[(crianca, fabrica)] for crianca in t_names for fabrica in fabricas_crianca[crianca] if crianca_to_pais[crianca] != pais and fabrica_to_pais[fabrica] == pais) <= m_vars[pais].upBound
+    problem += fabrica_to_stock[fabrica] <= fabrica_max[fabrica]
 
 # Função objetivo
-problem += p.lpSum(x[(crianca, fabrica)] for crianca in t_names for fabrica in fabricas_crianca[crianca])
+problem += funcao_objetivo
 
 problem.solve(p.PULP_CBC_CMD(msg=False))
 
